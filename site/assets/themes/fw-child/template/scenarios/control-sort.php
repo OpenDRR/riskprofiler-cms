d2b5b62f7d6a467f43c8e45df56dc1b7120d6e67
anthonyfok <anthonyfok@users.noreply.github.com>
2022-05-06 20:57:54 -0700

    deploy: b5704fba4e69c1bc3d86cc24478287b60e18c8d1

--- site/assets/themes/fw-child/template/scenarios/control-sort.php
+++ site/assets/themes/fw-child/template/scenarios/control-sort.php
@@ -1,26 +1,5 @@
 <div class="px-2 pt-2 pb-1 d-flex flex-wrap">
 	<div class="sort-item d-flex mr-1 mb-1 rounded-pill selected" data-sort-key="name" data-sort-order="asc">
-		<h6 class="mb-0 mr-2">Name</h6>
-		<span class=""><i class="fas fa-caret-up"></i></span>
-	</div>
-
-	<div class="sort-item d-flex mr-1 mb-1 rounded-pill" data-sort-key="magnitude" data-sort-order="asc">
-		<h6 class="mb-0 mr-2">Magnitude</h6>
-		<span class=""><i class="fas fa-caret-up"></i></span>
-	</div>
-
-	<div class="sort-item d-flex mr-1 mb-1 rounded-pill" data-sort-key="deaths" data-sort-order="asc">
-		<h6 class="mb-0 mr-2">Deaths</h6>
-		<span class=""><i class="fas fa-caret-up"></i></span>
-	</div>
-
-	<div class="sort-item d-flex mr-1 mb-1 rounded-pill" data-sort-key="damage" data-sort-order="asc">
-		<h6 class="mb-0 mr-2">Damage</h6>
-		<span class=""><i class="fas fa-caret-up"></i></span>
-	</div>
-
-	<div class="sort-item d-flex mr-1 mb-1 rounded-pill" data-sort-key="dollars" data-sort-order="asc">
-		<h6 class="mb-0 mr-2">Dollars</h6>
-		<span class=""><i class="fas fa-caret-up"></i></span>
-	</div>
+		<h6 class="mb-0 mr-2"></h6>
+</div>
 </div>
